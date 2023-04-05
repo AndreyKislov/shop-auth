31e17ce8e89ee39ba3e1feaf40eeadbeed96385e
Andrey <neznajkaandrej99@gmail.com>
2023-04-05 23:09:49 +0300

    Create project with all dependencies and fill application.properties

---
+++ src/main/java/ua/kislov/reg_and_auth_service/RegAndAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.kislov.reg_and_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegAndAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegAndAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kislov/reg_and_auth_service/RegAndAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kislov.reg_and_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegAndAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
